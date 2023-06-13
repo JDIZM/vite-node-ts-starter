1385cfe3ed7834feebd5d023f2461bc6f2b194ca
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2023-06-13 21:11:57 +0100

    feat: esbuild and tsc configs with vitest coverage

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
+#!/usr/bin/env node
+
 import { exampleFunction } from "./exampleFunction";
+import { version } from "../package.json";
+
+// importing package json makes tsc bundle the /src folder
+console.log("version", version);
 
 exampleFunction("foo", "bar");
 
