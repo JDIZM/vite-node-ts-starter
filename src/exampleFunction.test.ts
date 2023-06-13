a8d5847c66db6b7750690a9597753f7a156202bf
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2023-06-13 19:07:18 +0100

    chore: rm

--- src/exampleFunction.test.ts
+++ src/exampleFunction.test.ts
@@ -1,6 +1,5 @@
 import { describe, expect, it } from "vitest";
 import { exampleFunction } from "./exampleFunction";
-import exp from "constants";
 
 describe("exampleFunction", () => {
   it("should do something...", () => {
